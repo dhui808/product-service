3bbdb72053804ccefe84cd354cba1e5f0773a08a
dannyhui <huichunwuca@yahoo.com>
2020-07-31 19:45:41 -0400

    cloud properties

--- src/main/java/productservice/ProductServiceApplication.java
+++ src/main/java/productservice/ProductServiceApplication.java
@@ -2,12 +2,23 @@ package productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		
+		SpringApplicationBuilder builder = configuration(new SpringApplicationBuilder());
+		SpringApplication app = builder.build();
+		Environment env = app.run(args).getEnvironment();
+		System.out.println("appversion=" + env.getProperty("appversion"));
+		System.out.println("port=" + env.getProperty("server.port"));
+		System.out.println("context path=" + env.getProperty("server.servlet.contextPath"));
 	}
-
+	
+	private static SpringApplicationBuilder configuration(SpringApplicationBuilder builder) {
+        return builder.sources(ProductServiceApplication.class);
+    }
 }
